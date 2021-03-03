2c703a11eacd5034e09013acb2aeeffb3e7d8de7
NICOLAS <72447720+MRCnicolas@users.noreply.github.com>
2021-03-03 11:59:47 +0100

    Depot3mars

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,6 +8,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Auteur;
+use App\Entity\Editeur;
+use App\Entity\Fournisseur;
+use App\Entity\Genre;
+use App\Entity\Produit;
+
 class DashboardController extends AbstractDashboardController
 {
     /**
@@ -26,7 +32,13 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linktoDashboard('Accueil', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Mes BD');
+        yield MenuItem::linkToCrud('Produits', 'fas fa-tags', Produit::class);
+        yield MenuItem::linkToCrud('Auteurs', 'fas fa-tags', Auteur::class);
+        yield MenuItem::linkToCrud('Genres', 'fas fa-tags', Genre::class);
+        yield MenuItem::linkToCrud('Editeurs', 'fas fa-tags', Editeur::class);
+        yield MenuItem::linkToCrud('Fournisseurs', 'fas fa-tags', Fournisseur::class);
     }
 }
