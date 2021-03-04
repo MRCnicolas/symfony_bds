e4e5ddf42ccfda7677f721642241784b8bdd15bc
NICOLAS <72447720+MRCnicolas@users.noreply.github.com>
2021-03-04 15:28:50 +0100

    Depot4 m

--- src/Controller/BdController.php
+++ src/Controller/BdController.php
@@ -2,23 +2,52 @@
 
 namespace App\Controller;
 
-use App\Repository\AuteurRepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AuteurRepository;
+
 
 class BdController extends AbstractController
 {
+
+
+/**
+     * @Route("/bd", name="bd")
+     */
+    public function index(AuteurRepository $repo): Response
+    {
+        $auteurs = $repo->findall();
+
+        return $this->render('bd/index.html.twig', [
+            'controller_name' => 'Bdcontroller',
+            'auteurs' => $auteurs
+        ]);
+
+        }
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/", name="home")
      */
     public function home(AuteurRepository $repo): Response
     {
         $auteurs = $repo->findall();
 
         return $this->render('bd/home.html.twig', [
-            'title' => 'Bienvenue sur le site des Bd',
-            'auteurs' => $auteurs
+            'title' => 'Bienvenue sur le site des BD',
+            'age' => 35
+        ]);
+    }
+
+     /**
+     * @Route("/bd/livre/{id}", name="bd_show")
+     */
+    public function show($id, AuteurRepository $repo) {
+
+        $auteur = $repo->find($id);
+
+        return $this->render('bd/show.html.twig', [
+            'auteur' => $auteur
         ]);
     }
 }
