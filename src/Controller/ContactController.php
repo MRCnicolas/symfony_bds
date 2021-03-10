670e38b49e0cc01757dea32f00199c2f49f043c8
NICOLAS <72447720+MRCnicolas@users.noreply.github.com>
2021-03-10 16:14:25 +0100

    Depot10mars

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,18 +3,39 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $contactFormData = $form->getData();
+
+            $email = (new Email())
+                 ->from( $contactFormData['from'])
+                 ->to('nicolas.mercier00@orange.fr')
+                 ->subject('Time for Symfony Mailer')
+                 ->text($contactFormData['message']);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé');
+
+            return $this->redirectToRoute('contact');
+        }
 
         return $this->render('contact/index.html.twig', [
             'contact_form' => $form->createView(),
